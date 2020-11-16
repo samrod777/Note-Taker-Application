2abbfcbb2ef867b70002a44abd5711694d715b84
Sam Rodriguez <rodriguez_414@yahoo.com>
2020-11-16 14:27:04 -0800

    server working, html routes working

---
+++ server.js
@@ -0,0 +1,24 @@
+
+
+const express = require("express");
+
+// Tells node that we are creating an "express" server
+const app = express();
+
+// Initial port.
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Routes
+
+// require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// LISTENER 
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
