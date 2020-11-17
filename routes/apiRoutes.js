c8697cfb663209df6261758e1a091c22adfe5dc6
Sam Rodriguez <rodriguez_414@yahoo.com>
2020-11-17 00:26:10 -0800

    delete function working?

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,75 +1,44 @@
 const fs = require("fs");
-const dbjason = require("../db/db.json");
-const { v4: uuidv4 } = require('uuid');
+const dbjson = require("../db/db.json");
+const { v4: uuidv4 } = require("uuid");
+const path = require("path");
 
-
-
-module.exports = function(app) {
-  app.get("/api/notes", function(req, res) {
-    res.json(dbjason);
+module.exports = function (app) {
+  app.get("/api/notes", function (req, res) {
+    res.json(dbjson);
   });
 
-  app.post("/api/notes", function(req, res) {
+  app.post("/api/notes", function (req, res) {
+    const id = uuidv4();
     const newNote = {
-       title: req.body.title,
-       text: req.body.text,
-       noteID: req.body.id = uuidv4()
-    }
-    dbjason.push(newNote);
-    fs.writeFile("./db/db.json", JSON.stringify(dbjason), function(err) {
-        if(err) {
-            return console.log(err);
-        }
-        res.json(newNote);
-        console.log("The file was updated!");
-         })
-    });
+      title: req.body.title,
+      text: req.body.text,
+      id: id,
+    };
+    dbjson.push(newNote);
+    fs.writeFile(
+      path.join(__dirname, "../db/db.json"),
+      JSON.stringify(dbjson),
+      (err) => {
+        if (err) throw err;
+      }
+    );
+    res.send(newNote);
+  });
 
- 
-  };
+  app.delete("/api/notes/:id", function (req, res) {
+    const noteId = req.params.id;
+    const deleteNote = dbjson.findIndex((el) => el.id == noteId);
+    dbjson.splice(deleteNote, 1);
 
-//   app.delete("/api/notes", function(req, res) {
-//     var chosen = req.params.id;
+    fs.writeFile(
+      path.join(__dirname, "../db/db.json"),
+      JSON.stringify(dbjson),
+      function (err) {
+        if (err) throw err;
+      }
+    );
 
-//     console.log(chosen);
-  
-//     for (var i = 0; i < dbjason.length; i++) {
-//       if (chosen === id[i]) {
-//         return res.json(dbjason[i]);
-//       }
-//     }
-
-
-//     // req.body.id = uuidv4()
-//     // dbjason.push(req.body);
-//     // res.json(dbjason);
-//   });
-
-
-
-  
-
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
-
-  
-
-  
-
- 
-
-
-
-
-// The following API routes should be created:
-
-// GET /api/notes - Should read the db.json file and return all saved notes as JSON.
-
-// POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
-
-// DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
+    res.json(dbjson);
+  });
+};
