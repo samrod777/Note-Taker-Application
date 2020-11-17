cea060f57dd227d38782bc09ec2641b79d2e5200
Sam Rodriguez <rodriguez_414@yahoo.com>
2020-11-17 15:32:02 -0800

    trying to fix heroku

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,23 +1,16 @@
-// ===============================================================================
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-// ===============================================================================
 const path = require("path");
 
-
-
-module.exports = function(app) {
-
-  app.get("/notes", function(req, res) {
+module.exports = function (app) {
+  app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
 
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
   // If no matching route is found default to home
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 };
